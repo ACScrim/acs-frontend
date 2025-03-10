5fd71904d1951f632ec74dede1bbd6c2faa0d4f9
Ninon <ninon.jordao@outlook.fr>
2025-03-10 13:55:50 +0100

    Initialisation du front

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <img src="./assets/logo.png" class="logo vue" alt="Vue logo" />
+  </div>
+  <div>En cours de création</div>
+</template>
+
+<style scoped>
+.logo {
+  height: 23em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
