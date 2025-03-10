f7798011c2014867f2745405a0c255870648e178
Ninon <ninon.jordao@outlook.fr>
2025-03-10 20:38:48 +0100

    Changement message

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,7 @@
   >
     <h1 class="text-4xl text-pink-500">Connexion</h1>
     <button class="mt-4 px-4 py-2 bg-pink-500 text-white rounded">
-      Se connecter avec Discord
+      Se connecter avec Discord (à venir)
     </button>
   </div>
 </template>
