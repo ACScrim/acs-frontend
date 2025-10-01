c63c18abadb0867a4542ad5b49ff52294f282291
Tekninon <ninon.jordao@outlook.fr>
2025-10-01 16:55:28 +0200

    Correction erreur bouton

--- src/views/AuthError.vue
+++ src/views/AuthError.vue
@@ -40,7 +40,7 @@
       variant="error"
       :stars="true"
       :decorated="true"
-      className="max-w-md w-full z-10 relative backdrop-blur-sm"
+      className="max-w-md md:max-w-lg lg:max-w-2xl w-full z-10 relative backdrop-blur-sm"
     >
       <div class="text-center mb-6">
         <!-- Icône d'erreur avec effet de pulsation -->
@@ -53,7 +53,7 @@
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-12 w-12 text-color-error"
+              class="h-12 w-12 md:h-16 md:w-16 text-color-error"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -69,12 +69,16 @@
         </div>
 
         <!-- Titre de l'erreur -->
-        <Title size="2xl" :glitch="true" className="mb-4 text-color-error">
+        <Title
+          size="2xl"
+          :glitch="true"
+          className="mb-4 text-color-error lg:text-4xl"
+        >
           {{ title }}
         </Title>
 
         <!-- Message d'erreur -->
-        <p class="text-color-text mb-8">{{ message }}</p>
+        <p class="text-color-text mb-8 text-base md:text-lg">{{ message }}</p>
 
         <!-- Affichage spécifique pour l'erreur de serveur Discord -->
         <div v-if="errorType === 'guild_required'" class="space-y-6 mb-6">
@@ -92,35 +96,41 @@
             </div>
           </Terminal>
 
-          <Button
-            tag="a"
-            :href="inviteLink"
-            target="_blank"
-            rel="noopener noreferrer"
-            variant="primary"
-            size="lg"
-            className="mb-4"
-          >
-            <template #icon>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 mr-2"
-                viewBox="0 0 24 24"
-                fill="currentColor"
+          <div class="flex justify-center">
+            <a
+              :href="inviteLink"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="inline-block"
+            >
+              <Button
+                variant="primary"
+                size="lg"
+                :icon="true"
+                className="w-full lg:w-auto"
               >
-                <path
-                  d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z"
-                />
-              </svg>
-            </template>
-            REJOINDRE LE SERVEUR DISCORD
-          </Button>
+                <template #icon>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
+                    <path
+                      d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z"
+                    />
+                  </svg>
+                </template>
+                REJOINDRE LE SERVEUR DISCORD
+              </Button>
+            </a>
+          </div>
 
-          <p class="text-color-text-muted">
+          <p class="text-color-text-muted text-center lg:text-left">
             Après avoir rejoint le serveur Discord,
             <Button
               @click="retryAuth"
-              variant="ghost"
+              variant="outline"
               size="sm"
               className="ml-1"
             >
@@ -144,11 +154,38 @@
             </div>
           </Terminal>
 
+          <div class="flex justify-center mb-4">
+            <Button
+              @click="retryAuth"
+              variant="primary"
+              size="lg"
+              className="w-full lg:w-auto"
+            >
+              <template #icon>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </template>
+              RÉESSAYER LA CONNEXION
+            </Button>
+          </div>
+        </div>
+
+        <div class="flex justify-center">
           <Button
-            @click="retryAuth"
-            variant="primary"
-            size="lg"
-            className="mb-4"
+            @click="goHome"
+            variant="secondary"
+            size="md"
+            className="w-full lg:w-auto"
           >
             <template #icon>
               <svg
@@ -158,31 +195,13 @@
                 fill="currentColor"
               >
                 <path
-                  fill-rule="evenodd"
-                  d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
-                  clip-rule="evenodd"
+                  d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
                 />
               </svg>
             </template>
-            RÉESSAYER LA CONNEXION
+            RETOUR À L'ACCUEIL
           </Button>
         </div>
-
-        <Button @click="goHome" variant="ghost" size="md">
-          <template #icon>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5 mr-2"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
-              />
-            </svg>
-          </template>
-          RETOUR À L'ACCUEIL
-        </Button>
       </div>
     </Card>
 
